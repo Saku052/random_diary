9f09e72b08a6529f4d809df8165abac1a5b50a4a
Sora Harada <92529068+Saku052@users.noreply.github.com>
2024-09-08 20:00:08 +1000

    make framework for request diary

---
+++ lib/models/diary_model.dart
@@ -0,0 +1,9 @@
+class Diary {
+  final String name;
+  final String description;
+
+  const Diary({
+    required this.name,
+    required this.description,
+  });
+}
--- lib/views/diary_page.dart
+++ lib/views/diary_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:random_diary/widgets/top_banner.dart';
 import 'package:random_diary/widgets/text_box.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class DiaryPage extends StatefulWidget {
   const DiaryPage(this.changePage, {super.key});
